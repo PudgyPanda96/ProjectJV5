364bd1208ab4cc54b9223b2c87e911ec943689df
Aziz-Rah <ru.aziz.cs@gmail.com>
2020-03-16 17:17:09 -0400

    testttttttttttttttttttttttttttttttttttttttttttt

---
+++ BE/jv5-backend/src/main/java/com/jv5/jv5backend/Jv5BackendApplication.java
@@ -0,0 +1,13 @@
+package com.jv5.jv5backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jv5BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jv5BackendApplication.class, args);
+	}
+
+}
---
+++ BE/jv5-backend/src/test/java/com/jv5/jv5backend/Jv5BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jv5.jv5backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jv5BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
