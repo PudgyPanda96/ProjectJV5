d08d213849a515b2f60509d65d5052a81903faa4
Michael Serpico <10mvasta@gmail.com>
2020-05-17 19:15:58 -0400

    added exp id stuff

--- DB/experience_db_setup.js
+++ DB/experience_db_setup.js
@@ -4,12 +4,14 @@ var db = connect('localhost:27017/jv5')
 var data = [{
         "userId": "12345",
         "experiences": [{
+                "expId": "12345"
                 "game": "melee",
                 "description": "beat axe",
                 "year": "2019",
                 "event": "super smash con"
             },
             {
+                "expId": "12346",
                 "game": "valorant",
                 "description": "out fragged renard easily",
                 "year": "2020",
@@ -20,12 +22,14 @@ var data = [{
     {
         "userId": "23456",
         "experiences": [{
+                "expId": "12347",
                 "game": "melee",
                 "description": "beat nation",
                 "year": "2019",
                 "event": "fireside"
             },
             {
+                "expId": "12348",
                 "game": "booty eater sim",
                 "description": "ate that booty like groceries",
                 "year": "2020",
@@ -36,6 +40,7 @@ var data = [{
     {
         "userId": "34567",
         "experiences": [{
+            "expId": "12349",
             "game": "valorant",
             "description": "got plat",
             "year": "2020",
@@ -45,12 +50,14 @@ var data = [{
     {
         "userId": "45678",
         "experiences": [{
+                "expId": "22345",
                 "game": "Overwatch",
                 "description": "Coached rutgers esl team",
                 "year": "2018",
                 "event": "ESL"
             },
             {
+                "expId": "32345",
                 "game": "Halo",
                 "description": "fucked some nerds",
                 "year": "2020",
